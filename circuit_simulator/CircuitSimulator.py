492ddbe82a9fdd5d3addf17295df8437c0f28cc6
Nathan Gurrin-Smith <nathan.gurrinsmith@outlook.com>
2024-04-14 07:14:06 -0400

    Pre-computing some gate multiplication and utlizing njit for probability stuff.

--- circuit_simulator/CircuitSimulator.py
+++ circuit_simulator/CircuitSimulator.py
@@ -1,8 +1,26 @@
 from circuit import QuantumCircuit, MeasurementOp, GateOp
-from numba import njit, cuda
+from numba import njit, cuda, prange
 import numpy as np
 import math
 from utils import numba_matmul, cu_left_mul, cu_right_mul, make_copy, cu_trace, cu_right_mul_trace
+
+
+def combine_gates(gates: list) -> np.array:
+    """
+    Combines each gate in gates by repeatedly computing gates[i+1] @ gates[i].
+    For example, with two consecutive gates G_1 and G_2, it should return G_2 G_1.
+
+    === Prerequisites ===
+    - gates[i].shape[1] == gates[i+1].shape[0] for all i
+    - len(gates) >= 1
+    """
+    if len(gates) < 1:
+        raise ValueError("Please provide a list with at least one gate!")
+    result = np.eye(gates[0].shape[0])
+    for gate in gates:
+        result = gate @ result
+    return result
+
 
 class CircuitSimulator:
     """
@@ -41,17 +59,20 @@ class CircuitSimulator:
         ops = []
         classical_store = []
         state = self.circuit.reg.state.get_state()
-
+        temp_gate_ops = []
         for op in self.circuit.ops:
             if isinstance(op, GateOp):
-                ops.append(op.gate.matrix_rep())
-                classical_store.append(-1)
+                temp_gate_ops.append(op.gate.matrix_rep())
             elif isinstance(op, MeasurementOp):
+                if temp_gate_ops != []:
+                    ops.append(combine_gates(temp_gate_ops))
+                    temp_gate_ops = []
+                    classical_store.append(-1)
                 ops.append(self.circuit.reg.get_zero_projector(op.target, self.circuit.reg.num_qubits))
                 classical_store.append(op.classical_store)
         
         return np.array(ops, dtype=np.complex64), state, np.array(classical_store, dtype=np.int8)
-
+    
     def compute_measurements(self, probs: np.array):
         """
         Computes the classical measurements using the probs array.
@@ -96,7 +117,8 @@ class NumbaSimulator(CircuitSimulator):
 
         self._run(ops, cs, state, self.shots, state_out, cs_out)
         self.state_result = state_out
-        self.compute_measurements(cs_out)
+        #self.compute_measurements(cs_out)
+        self.cs_result = compute_measurements(cs_out)
         return state_out, cs_out
         
 
@@ -118,7 +140,7 @@ class NumbaSimulator(CircuitSimulator):
         - state_out.shape == (shots,) + state.shape
         - cs_out.shape == (shots,) + (max(cs),) + (2,)
         """
-        for shot in range(shots):
+        for shot in prange(shots):
             state_out[shot] = state
             for i in range(len(ops)):
                 if cs[i] == -1:  # this is a gate
@@ -133,6 +155,36 @@ class NumbaSimulator(CircuitSimulator):
                     cs_out[shot, cs[i], 0] = zero_prob
                     cs_out[shot, cs[i], 1] = one_prob
         
+@njit
+def sample_shots(trace: np.array, out_idx: int, out: np.array) -> None:
+    for shot in prange(trace.shape[0]):
+        zero_prob = float(max(trace[shot].real, 0.0))
+        out[shot, out_idx, 0] = zero_prob
+        out[shot, out_idx, 1] = 1 - zero_prob
+
+
+@njit
+def rand_choice_nb(arr, prob):
+    """
+    Computes np.random.choice with a specified p=
+    Numba doesn't allow the use of the p= argument to random.choice.
+    Workaround: https://github.com/numba/numba/issues/2539#issuecomment-507306369
+
+    === Parameters ===
+    - arr: array to sample values from
+    - prob: probabilities of each sample value
+    """
+    return arr[np.searchsorted(np.cumsum(prob), np.random.random(), side="right")]
+
+
+@njit
+def compute_measurements(probs: np.array) -> np.array:
+    out = np.zeros((probs.shape[0], probs.shape[1]), dtype=np.int8)
+    for i in prange(out.shape[0]):
+        for j in prange(out.shape[1]):
+            out[i, j] = rand_choice_nb([0, 1], probs[i, j])
+    return out
+
 
 class CUDASimulator(CircuitSimulator):
     def run(self):
@@ -176,13 +228,8 @@ class CUDASimulator(CircuitSimulator):
             else:  # this is a measurement
                 op = cuda.to_device(ops[i].T)
                 cu_right_mul_trace[trace_blockspergrid, trace_threadsperblock](op, state_out_d, trace_output_d)
-                #cu_right_mul[blockspergrid, threadsperblock](op, state_out_d, output_tensor_d)
-                #cu_trace[trace_blockspergrid, trace_threadsperblock](output_tensor_d, trace_output_d)
-                X = trace_output_d.copy_to_host() 
-                for shot in range(X.shape[0]):
-                    zero_prob = float(max(X[shot].real, 0.0))
-                    cs_out[shot, cs[i], 0] = zero_prob
-                    cs_out[shot, cs[i], 1] = 1 - zero_prob
+                X = trace_output_d.copy_to_host()
+                sample_shots(X, cs[i], cs_out)
         self.state_result = state_out
-        self.compute_measurements(cs_out)
+        self.cs_result = compute_measurements(cs_out)
         return state_out, cs_out
